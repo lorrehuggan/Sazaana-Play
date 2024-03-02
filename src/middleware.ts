8f924d6d82bfec66e6e8f7d1cb61201a355bf3fd
Lorre <lorre.huggan@protonmail.com>
2024-03-02 17:26:07 +0000

    chore: update middleware to include '/login' path in the matcher configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,16 @@
-import { auth, validateRequest } from './lib/auth';
-import { redirect } from 'next/navigation';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  NextResponse.next();
+  const response = NextResponse.next();
+
+  return response;
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
     // '/((?!api|_next/static|_next/image|favicon.ico).*)',
     '/playlist/:path*',
+    '/login/:path*',
   ],
 };
