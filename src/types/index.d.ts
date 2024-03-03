6184edecdafe2be9fb354245ac5d7c5d00ac357e
Lorre <lorre.huggan@protonmail.com>
2024-03-03 00:41:26 +0000

    feat: add Artist interface to define the structure of an artist object
    The Artist interface includes properties such as external_urls, followers, genres, href, id, images, name, popularity, type, and uri. This interface is used to represent an artist object in the application.

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -91,6 +91,19 @@ export interface Item {
     uri: string;
 }
 
+export interface Artist {
+    external_urls: ExternalUrls;
+    followers: Followers;
+    genres: string[];
+    href: string;
+    id: string;
+    images: Image[];
+    name: string;
+    popularity: number;
+    type: string;
+    uri: string;
+}
+
 export interface ExternalUrls {
     spotify: string;
 }
