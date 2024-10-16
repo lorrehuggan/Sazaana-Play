03242ca568099fa3cd75ce3048d2e2de348cfab9
Lorre <lorre.huggan@protonmail.com>
2024-10-16 17:31:18 +0100

    fix everything that was broken

--- src/lib/service/redis/limiters.ts
+++ src/lib/service/redis/limiters.ts
@@ -2,17 +2,17 @@ import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 export const searchLimiter = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(5, "10 s"),
-  analytics: true,
-  timeout: 1000 * 60 * 2, // 2 minute timeout
-  prefix: "search",
+	redis: Redis.fromEnv(),
+	limiter: Ratelimit.slidingWindow(5, "10 s"),
+	analytics: true,
+	timeout: 1000 * 60 * 2, // 2 minute timeout
+	prefix: "search",
 });
 
 export const filterLimiter = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-  analytics: true,
-  timeout: 1000 * 60 * 4, // 4 minute timeout
-  prefix: "create-playlist",
+	redis: Redis.fromEnv(),
+	limiter: Ratelimit.slidingWindow(10, "10 s"),
+	analytics: true,
+	timeout: 1000 * 60 * 4, // 4 minute timeout
+	prefix: "create-playlist",
 });
